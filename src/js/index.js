1875101aa200bee27ce70fd97280e79708031e16
ZhengJie <jabarizheng@gmail.com>
2025-09-11 02:00:22 +0800

    Redesign homepage layout and assets

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
-let navShow = true
+let navShow = false;
 let enterBtn = document.getElementById("enterBtn");
 console.log('enterBtn', enterBtn);
 
+let descContainerChildEle = document.getElementById("desc-container-child");
 let navContainer = document.querySelector(".nav-container");
 enterBtn.addEventListener("click", () => {
   navContainer.setAttribute('class', `nav-container ${enterBtn ? 'hide' : ''}`)
+  descContainerChildEle.setAttribute('class', `container ${enterBtn ? '' : 'nav-show'}`)
   enterBtn = !enterBtn;
 });
